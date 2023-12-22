e32a77080bd2bec2dac6d00e46cb46bf04c4f081
Dương ITUS <duongdeep@gmail.com>
2023-12-23 02:14:27 +0700

    product page

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,10 +1,21 @@
 ﻿import { useParams } from "react-router-dom";
-
+import { Row, Col } from "antd";
+import ProductPage from "../pages/ProductPage";
 const ProductDetailPage = () => {
   const { id } = useParams();
 
     return (
-        <div>hello {id}</div>
+        <div>
+            
+                        <ProductPage
+                            id={id.id}
+                            cover={id.cover}
+                            title={id.title}
+                            subtitle={id.subtitle}
+                            description={id.description}
+                        />
+                    
+        </div>
     );
 };
 
