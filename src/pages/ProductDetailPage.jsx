a07b9134a119749740d236cc3e8de3e5a54eed0d
nhngoccham <nhnt1811@gmail.com>
2023-12-28 01:22:55 +0700

    infor input

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,10 +1,51 @@
 ﻿import { useParams } from "react-router-dom";
 import { Row, Col } from "antd";
 import ProductPage from "../pages/ProductPage";
-const ProductDetailPage = () => {
-  const { id } = useParams();
+import { Layout, Menu, theme, Card } from 'antd';
+import { Descriptions, Collapse } from 'antd';
+import { Content } from 'antd/es/layout/layout';
 
-  return <div>heello {id}</div>;
+const { Header } = Layout;
+
+const items_renamed = [
+  {
+    key: '10',
+    label: 'Quyền lợi của người sử dụng',
+    children: <p>aaaaaaaaaaaaaaaaa</p>,
+    
+  },
+  {
+    key: '11',
+    label: 'Điều kiện tham gia',
+    children: <p>bbbbbbbbbbbbbbb</p>,
+  },
+  {
+    key: '12',
+    label: 'Điều khoản loại trừ',
+    children: <p></p>,
+  }
+];
+const ProductDetailPage = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{height: "100%"}}>
+      <Header style={{ padding: 38, background: colorBgContainer, display: 'flex', alignItems: 'center' }}>
+          <span style={{ justifyContent: 'center', fontSize: 26, fontWeight: 'bold' }}>Thông tin bảo hiểm đã đăng ký</span>
+        </Header>
+        <Content style={{margin: 40}}>
+          <Card
+            style={{ width: "100%", maxWidth: 10000, minHeight: "100vh" }}
+            bodyStyle={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+          >
+            {/* Corrected usage of Collapse */}
+            <Collapse accordion items={items_renamed} style={{ width: '100%', border: 'none' }} />
+          </Card>
+        </Content>
+    </Layout>
+  );
 };
 
 export default ProductDetailPage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -11,6 +11,7 @@ import ProductPage from "../pages/ProductPage";
 import ProductDetailPage from "../pages/ProductDetailPage";
 import SupportPage from "../pages/SupportPage";
 import ProfileLayout from "../layouts/ProfileLayout";
+import InputInforPage from "../pages/InputInforPage";
 
 const ProfileRouter = () => {
   return (
@@ -38,6 +39,7 @@ const Router = () => {
           <Route path="/support" element={<SupportPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/infor" element={<InputInforPage />} />
           <Route path="/profile/*" element={<ProfileRouter />} />
           {/* <Route path="/profile" element={<ProfilePage />} />
                     <Route path="/profile_update" element={<ProfilePage_update />} />
