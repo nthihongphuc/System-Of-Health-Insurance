ed43c15175da46dc1baaa00d297510586a120419
Dương ITUS <duongdeep@gmail.com>
2024-01-24 01:50:05 +0700

    fix ui type+ requets payment

--- src/pages/RequestPayment.jsx
+++ src/pages/RequestPayment.jsx
@@ -14,11 +14,13 @@ import {
   Radio,
   message,
   Upload,
+  Space,
+  Typography
 } from "antd";
 import { Content } from "antd/es/layout/layout";
 import { address } from "../data/address";
 import { useNavigate } from "react-router-dom";
-
+const { Text } = Typography;
 
 const props = {
   name: "file",
@@ -39,13 +41,19 @@ const props = {
 };
 
 const RequestPayment = () => {
-  // const [componentDisabled, setComponentDisabled] = useState(true);
+  const [componentDisabled, setComponentDisabled] = useState(true);
   const navigate = useNavigate();
   const {
     token: { colorBgContainer },
   } = theme.useToken();
   const onFinish = (values) => {
     console.log(values);
+  };
+  
+  const [value, setValue] = useState(1);
+  const onChange = (e) => {
+    console.log('radio checked', e.target.value);
+    setValue(e.target.value);
   };
   return (
     <Layout style={{ height: "100%" }}>
@@ -156,23 +164,79 @@ const RequestPayment = () => {
               </Upload>
             </Card>
             {/* Nút để qua trang chọn hình thức thanh toán */}
-            <Button
-              type="primary"
-              className="request-payment-button"
-              style={{ marginTop: 10 }}
-              onClick={() => {
-                if (!localStorage.getItem("access_token")) {
-                  navigate("/login");
-                } else {
-                  navigate(`/type_payment`);
-                }
+            <Form.Item
+              style={{ width: "100%"}}
+              bodyStyle={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
               }}
+              label="Chọn hình thức thanh toán"
+              name="type-payment"
+              rules={[
+                {
+                  required: true,
+                  message: "Vui lòng chọn hình thức thanh toán!",
+                },
+              ]}
             >
-              <div>Chọn hình thức thanh toán</div>
-            </Button>
-            {/* <Button type="primary" htmlType="submit" style={{ marginTop: 10 }}>
-              Chọn hình thức thanh toán
-            </Button> */}
+              <Radio.Group onChange={onChange} value={value}>
+                <Space direction="vertical">
+                  <Radio value={1}>Ngân hàng</Radio>
+                  <Radio value={2}>MoMo</Radio>
+                  <Radio value={3}>Tiền mặt</Radio>
+                  <Radio value={4}>Khác</Radio>
+                    {value === 1 ? (
+                      <Input
+                      placeholder="Ngân hàng - Số tài khoản (VD: MB Bank - 0909090909)"
+                        style={{
+                          width: 550,
+                          marginLeft: 10,
+                        }}
+                      />
+                    ) : null}
+                    {value === 2 ? (
+                      <Input
+                      placeholder="MoMo - Số điện thoại (VD: MoMo - 0909090909)"
+                        style={{
+                          width: 550,
+                          marginLeft: 10,
+                        }}
+                      />
+                    ) : null}
+                    {value === 3 ? (
+                      <Text italic
+                      style={{
+                        marginLeft: 10,
+                      }}
+                       > 
+                      <p>Khách hàng đến nhận trực tiếp tại công ty. Địa chỉ: 12 Quang Trung, Q6.</p>
+                      </Text>
+                    ) : null}
+                    {value === 4 ? (
+                      <Input
+                        placeholder="Nhập yêu cầu khác"
+                        style={{
+                          width: 550,
+                          marginLeft: 10,
+                        }}
+                      />
+                    ) : null}
+                </Space>
+              </Radio.Group>
+              
+            </Form.Item>
+            {/* Nút gửi yêu cầu */}
+            <Button
+                type="primary"
+                htmlType="submit"
+                className="request-payment-form-button"
+                style={{ marginTop: 10 }}
+              >
+                Gửi yêu cầu thanh toán
+              </Button>
+            
+
           </Form>
         </Card>
       </Content>
